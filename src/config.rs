faeb3a5876ed28fbdbb69ec60d3842306f1f8ffc
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-02-13 15:41:10 -0300

    Added border width config

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,7 @@
 pub struct Config {
     pub workspaces: &'static [&'static str],
     pub mouse_mod: &'static [&'static str],
+    pub border_width: u32,
 }
 
 impl Config {
@@ -9,6 +10,7 @@ impl Config {
         Config {
             workspaces: &["1", "2", "3", "4", "5", "6", "7", "8", "9"],
             mouse_mod: &["Super"],
+            border_width: 4,
         }
     }
 }
